a56eb515617fc8afde380bf4feb387f36cd65e7f
JI Villalobos <chuy12villalobos@gmail.com>
2023-02-02 14:18:18 -0600

    add expense and income types dtos

---
+++ src/main/java/com/jiltsa/admin/cashproof/domain/dto/ExpenseTypeDto.java
@@ -0,0 +1,13 @@
+package com.jiltsa.admin.cashproof.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ExpenseTypeDto {
+    private Integer id;
+    private String type;
+}
---
+++ src/main/java/com/jiltsa/admin/cashproof/domain/dto/IncomeTypeDto.java
@@ -0,0 +1,14 @@
+package com.jiltsa.admin.cashproof.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class IncomeTypeDto {
+    private Integer id;
+    private String type;
+
+}
