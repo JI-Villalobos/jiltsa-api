34af695b53996f79ee7e7fa6dc65968c8680d722
JI Villalobos <chuy12villalobos@gmail.com>
2023-02-06 14:42:43 -0600

    add income registry mapper

---
+++ src/main/java/com/jiltsa/admin/cashproof/persistence/mapper/IncomeRegistryMapper.java
@@ -0,0 +1,26 @@
+package com.jiltsa.admin.cashproof.persistence.mapper;
+
+import com.jiltsa.admin.cashproof.domain.dto.CreateIncomeRegistryDto;
+import com.jiltsa.admin.cashproof.domain.dto.IncomeRegistryDto;
+import com.jiltsa.admin.cashproof.persistence.entity.IncomeRegistry;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface IncomeRegistryMapper {
+    IncomeRegistryDto toIncomeRegistryDto(IncomeRegistry incomeRegistry);
+    List<IncomeRegistryDto> toIncomeRegistryDtoList(List<IncomeRegistry> incomeRegistryList);
+    CreateIncomeRegistryDto toCreateIncomeRegistryDto(IncomeRegistry incomeRegistry);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "time", ignore = true),
+            @Mapping(target = "accounting", ignore = true),
+            @Mapping(target = "incomeType", ignore = true)
+    })
+    IncomeRegistry toIncomeRegistry(CreateIncomeRegistryDto createIncomeRegistryDto);
+}
