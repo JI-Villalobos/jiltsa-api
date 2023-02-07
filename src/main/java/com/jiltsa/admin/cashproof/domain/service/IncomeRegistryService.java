fe7e182a136cf57c4cf3b04758b5ca0a3b46460f
JI Villalobos <chuy12villalobos@gmail.com>
2023-02-06 18:27:48 -0600

    add income registry service

---
+++ src/main/java/com/jiltsa/admin/cashproof/domain/service/IncomeRegistryService.java
@@ -0,0 +1,24 @@
+package com.jiltsa.admin.cashproof.domain.service;
+
+import com.jiltsa.admin.cashproof.domain.dto.CreateExpenseRegistryDto;
+import com.jiltsa.admin.cashproof.domain.dto.CreateIncomeRegistryDto;
+import com.jiltsa.admin.cashproof.domain.dto.IncomeRegistryDto;
+import com.jiltsa.admin.cashproof.domain.repository.IncomeRegistryDRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class IncomeRegistryService {
+    private final IncomeRegistryDRepository incomeRegistryDRepository;
+
+    public List<IncomeRegistryDto> getIncomeRegistries(Integer accountingId){
+        return incomeRegistryDRepository.getIncomeRegistries(accountingId);
+    }
+
+    public CreateIncomeRegistryDto createIncomeRegistry(CreateIncomeRegistryDto createIncomeRegistryDto){
+        return incomeRegistryDRepository.createIncomeRegistry(createIncomeRegistryDto);
+    }
+}
