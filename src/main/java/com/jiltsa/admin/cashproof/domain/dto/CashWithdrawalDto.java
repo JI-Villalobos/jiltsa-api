ff28e97120fad8972ad110096d043adfed928dc2
JI Villalobos <chuy12villalobos@gmail.com>
2023-02-11 15:03:36 -0600

    add cash withdrawal dtos

---
+++ src/main/java/com/jiltsa/admin/cashproof/domain/dto/CashWithdrawalDto.java
@@ -0,0 +1,17 @@
+package com.jiltsa.admin.cashproof.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CashWithdrawalDto {
+    private Integer id;
+    private LocalDateTime date;
+    private Double amount;
+    private String concept;
+}
