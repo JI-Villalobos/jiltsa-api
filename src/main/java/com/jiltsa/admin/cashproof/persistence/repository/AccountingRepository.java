6527b1850faa522dd14569ee2a8ee39f5f749abe
JI Villalobos <chuy12villalobos@gmail.com>
2023-03-05 13:14:23 -0600

    delete accounting repo's unused imports

--- src/main/java/com/jiltsa/admin/cashproof/persistence/repository/AccountingRepository.java
+++ src/main/java/com/jiltsa/admin/cashproof/persistence/repository/AccountingRepository.java
@@ -1,15 +1,13 @@
 package com.jiltsa.admin.cashproof.persistence.repository;
 
 import com.jiltsa.admin.cashproof.persistence.entity.Accounting;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AccountingRepository extends JpaRepository<Accounting, Integer> {
+    //TODO:should find by branch too
     List<Accounting> findTop20ByOrderByIdDesc();
     List<Accounting> findByDateBetween(LocalDateTime start, LocalDateTime end);
 }
