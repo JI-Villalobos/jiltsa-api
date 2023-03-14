6f36414cbb2ba431c45e7a3ad9b14a984fece098
JI Villalobos <chuy12villalobos@gmail.com>
2023-03-13 21:22:03 -0600

    add income-expenses-rg controllers

---
+++ src/main/java/com/jiltsa/admin/cashproof/controller/ExpenseRegistryController.java
@@ -0,0 +1,26 @@
+package com.jiltsa.admin.cashproof.controller;
+
+import com.jiltsa.admin.cashproof.domain.dto.CreateExpenseRegistryDto;
+import com.jiltsa.admin.cashproof.domain.dto.ExpenseRegistryDto;
+import com.jiltsa.admin.cashproof.domain.service.ExpenseRegistryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/expenses")
+@RequiredArgsConstructor
+public class ExpenseRegistryController {
+    private final ExpenseRegistryService service;
+
+    @GetMapping("/{accountingId}")
+    public List<ExpenseRegistryDto> getExpenseRegistries(@PathVariable("accountingId") Integer accountingId){
+        return service.getExpenseRegistries(accountingId);
+    }
+
+    @PostMapping
+    public CreateExpenseRegistryDto createExpenseRegistry(@RequestBody CreateExpenseRegistryDto createExpenseRegistryDto){
+        return service.createExpenseRegistry(createExpenseRegistryDto);
+    }
+}
---
+++ src/main/java/com/jiltsa/admin/cashproof/controller/IncomeRegistryController.java
@@ -0,0 +1,27 @@
+package com.jiltsa.admin.cashproof.controller;
+
+import com.jiltsa.admin.cashproof.domain.dto.CreateIncomeRegistryDto;
+import com.jiltsa.admin.cashproof.domain.dto.IncomeRegistryDto;
+import com.jiltsa.admin.cashproof.domain.service.IncomeRegistryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/incomes")
+@RequiredArgsConstructor
+public class IncomeRegistryController {
+
+    public final IncomeRegistryService service;
+    @GetMapping("/{accountingId}")
+    public List<IncomeRegistryDto> getIncomeRegistries(@PathVariable("accountingId") Integer accountingId){
+        return service.getIncomeRegistries(accountingId);
+    }
+
+    @PostMapping
+    public CreateIncomeRegistryDto createIncomeRegistry(@RequestBody CreateIncomeRegistryDto createIncomeRegistryDto){
+        return service.createIncomeRegistry(createIncomeRegistryDto);
+    }
+
+}
