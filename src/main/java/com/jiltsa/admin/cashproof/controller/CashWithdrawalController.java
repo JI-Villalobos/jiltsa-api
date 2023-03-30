f750fd78f6d819c8887700809136ed36004f73e6
JI Villalobos <chuy12villalobos@gmail.com>
2023-03-29 18:05:02 -0600

    add cash withdrawals controller

---
+++ src/main/java/com/jiltsa/admin/cashproof/controller/CashWithdrawalController.java
@@ -0,0 +1,27 @@
+package com.jiltsa.admin.cashproof.controller;
+
+import com.jiltsa.admin.cashproof.domain.dto.CashWithdrawalDto;
+import com.jiltsa.admin.cashproof.domain.dto.CreateCashWithdrawalDto;
+import com.jiltsa.admin.cashproof.domain.service.CashWithdrawalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/withdrawals")
+@RequiredArgsConstructor
+public class CashWithdrawalController {
+    private final CashWithdrawalService service;
+
+    @GetMapping("/{branch}")
+    public List<CashWithdrawalDto> getCashWithdrawals(@PathVariable("branch") String branch){
+        return service.getCashWithdrawals(branch);
+    }
+
+    @PostMapping
+    public CreateCashWithdrawalDto createCashWithdrawalDto(@RequestBody CreateCashWithdrawalDto createCashWithdrawalDto){
+        return service.createCashWithdrawal(createCashWithdrawalDto);
+    }
+
+}
