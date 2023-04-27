9569517284106a21ebf2db7b476ad8c85299def0
JI Villalobos <chuy12villalobos@gmail.com>
2023-04-26 21:29:27 -0600

    cors header

--- src/main/java/com/jiltsa/admin/security/config/CorsConfig.java
+++ src/main/java/com/jiltsa/admin/security/config/CorsConfig.java
@@ -7,14 +7,16 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH"));
+        configuration.setExposedHeaders(List.of("Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
