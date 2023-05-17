7feefb590881bb787e3df1c4416464abd29e3617
JI Villalobos <chuy12villalobos@gmail.com>
2023-05-17 08:21:40 -0600

    refactor seller service tests

--- src/test/java/com/jiltsa/admin/seller/persistence/repository/SellerRepositoryTest.java
+++ src/test/java/com/jiltsa/admin/seller/persistence/repository/SellerRepositoryTest.java
@@ -9,15 +9,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SellerRepositoryTest {
     @Autowired
     private SellerRepository sellerRepositoryUnderTest;
@@ -27,7 +29,7 @@ class SellerRepositoryTest {
     void setUp() {
         List<Seller> sellers = new ArrayList<>();
         List<Accounting> accountingList = new ArrayList<>();
-        Branch branch = new Branch(1, "none", true, sellers, accountingList);
+        Branch branch = new Branch(1, "CENTRO", true, sellers, accountingList);
         branchRepository.save(branch);
 
         Seller seller = new Seller(1, "Diana Path", 1, "1234", true, branch, accountingList);
