647fe89894061c4bb6e214db3c1e15ebc1e7354d
Arche <chuy12villalobos@gmail.com>
2026-03-28 16:44:41 -0600

    update repositories

--- src/main/java/com/jiltsa/admin/cashproof/persistence/repository/ExpenseRegistryRepository.java
+++ src/main/java/com/jiltsa/admin/cashproof/persistence/repository/ExpenseRegistryRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface ExpenseRegistryRepository extends JpaRepository<ExpenseRegistry, Integer> {
     List<ExpenseRegistry> findByAccountingId(Integer accountingId);
+    List<ExpenseRegistry> findByExpenseTypeIdIn(List<Integer> expenseTypeIds);
 }
