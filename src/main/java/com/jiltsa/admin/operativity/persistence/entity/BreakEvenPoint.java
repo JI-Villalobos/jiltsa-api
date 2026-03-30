6eb7a88fbe04ac68358389ffd2cc05e7bd5555cf
JI-Villalobos <chuy12villalobos@gmail.com>
2026-03-30 13:47:50 -0600

    add pe entitiy

---
+++ src/main/java/com/jiltsa/admin/operativity/persistence/entity/BreakEvenPoint.java
@@ -0,0 +1,56 @@
+package com.jiltsa.admin.operativity.persistence.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "break_even_point")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BreakEvenPoint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private Double paysheet;
+
+    @NotNull
+    private Double rent;
+
+    @NotNull
+    @Column(name = "electricity_service")
+    private Double electricityService;
+
+    @NotNull
+    private Double internet;
+
+    @NotNull
+    @Column(name = "other_services")
+    private Double otherServices;
+
+    @NotNull
+    @Column(name = "gasoline_and_transport")
+    private Double gasolineAndTransport;
+
+    @NotNull
+    @Column(name = "admin_payment")
+    private Double adminPayment;
+
+    @NotNull
+    @Column(name = "operating_cost")
+    private Double operatingCost;
+
+    @NotNull
+    @Column(name = "fixed_expenses")
+    private Double fixedExpenses;
+
+    @NotNull
+    private Double pe;
+}
