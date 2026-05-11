2267b0fb891c2f76c21ae012dc9ceb0cf3905013
Arche <chuy12villalobos@gmail.com>
2026-05-11 15:49:41 -0600

    update cash-sorting entity with not-null condition

--- src/main/java/com/jiltsa/admin/cashproof/persistence/entity/CashSorting.java
+++ src/main/java/com/jiltsa/admin/cashproof/persistence/entity/CashSorting.java
@@ -1,6 +1,7 @@
 package com.jiltsa.admin.cashproof.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,59 +21,78 @@ public class CashSorting {
     private Integer id;
 
     @Column(name = "branch_id")
+    @NotNull
     private Integer branchId;
 
     @Column(name = "accounting_id")
+    @NotNull
     private Integer accountingId;
 
     @Column(name = "cash_date")
+    @NotNull
     private LocalDateTime cashDate;
 
     @Column(name = "bt_1000")
+    @NotNull
     private Integer bt1000;
 
     @Column(name = "bt_500")
+    @NotNull
     private Integer bt500;
 
     @Column(name = "bt_200")
+    @NotNull
     private Integer bt200;
 
     @Column(name = "bt_100")
+    @NotNull
     private Integer bt100;
 
     @Column(name = "bt_50")
+    @NotNull
     private Integer bt50;
 
     @Column(name = "bt_20")
+    @NotNull
     private Integer bt20;
 
     @Column(name = "md_20")
+    @NotNull
     private Integer md20;
 
     @Column(name = "md_10")
+    @NotNull
     private Integer md10;
 
     @Column(name = "md_5")
+    @NotNull
     private Integer md5;
 
     @Column(name = "md_2")
+    @NotNull
     private Integer md2;
 
     @Column(name = "md_1")
+    @NotNull
     private Integer md1;
 
     @Column(name = "md_005")
+    @NotNull
     private Integer md005;
 
     @Column(name = "bls_10")
+    @NotNull
     private Integer bls10;
 
     @Column(name = "bls_5")
+    @NotNull
     private Integer bls5;
 
     @Column(name = "bls_2")
+    @NotNull
     private Integer bls2;
 
     @Column(name = "bls_1")
+    @NotNull
     private Integer bls1;
 }
